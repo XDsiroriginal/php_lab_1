02f77d1c38ceee297afb102b7c70bb25b867529b
XDsiroriginal <anna.sawhenko1984@gmail.com>
2026-03-06 18:09:48 +0700

    types.php

---
+++ types.php
@@ -0,0 +1,32 @@
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        padding: 100px;
+        font-size: 20px;
+    }
+
+</style>
+
+<?php
+    $_int = 5;
+    $_float = 5.5;
+    $_string = "hello ttit";
+    $_bool = true;
+    $_null = null;
+
+    $_numberString = "123";
+
+    echo gettype($_int) . ": " . $_int . "<br>" . gettype($_float) . ": " . $_float . "<br>" . gettype($_string) . ": " . $_string . "<br>" . gettype($_bool) . ": " . $_bool . "<br>" . gettype($_null) . ": " . $_null;
+    echo "<br><br> неявное преобразование: " . $_numberString + $_int;
+    echo "<br> явное преобразование: " . (int)$_float + $_int;
+    echo "<br><br> Проверка, существует ли переменная: " . (isset($_noIsSet) ? 'есть' : 'нету');
+    $_noIsSet = 'isSet';
+    echo "<br> emty(): " . (empty($_noIsSet) ? 'нету' : $_noIsSet);
+    unset($_noIsSet);
+    echo "<br> unset(): " . (empty($_noIsSet) ? 'нету' : $_noIsSet);
+
